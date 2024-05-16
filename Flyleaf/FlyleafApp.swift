ca91782c0b8b38337daf1d10a3f6034efc727cc9
Ben Cardy <ben.cardy@ocado.com>
2024-05-16 16:05:45 +0100

    Initial Commit

---
+++ Flyleaf/FlyleafApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlyleafApp.swift
+//  Flyleaf
+//
+//  Created by Ben Cardy on 16/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlyleafApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
